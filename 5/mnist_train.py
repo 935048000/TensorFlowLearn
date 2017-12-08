78e8bd6da8a699efb9b3f5e20fb5619030f9a288
vast <935048000@qq.com>
2017-12-08 20:14:01 +0800

    完整的MNIST识别，神经网络的定义、训练、测试分离。

--- 5/mnist_train.py
+++ 5/mnist_train.py
@@ -27,7 +27,7 @@ def train(mnist):
     # 定义损失函数、学习率、滑动平均操作以及训练过程。
     variable_averages = tf.train.ExponentialMovingAverage (MOVING_AVERAGE_DECAY, global_step)
     variables_averages_op = variable_averages.apply (tf.trainable_variables ())
-    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits (y, tf.argmax (y_, 1))
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits (logits=y, labels=tf.argmax (y_, 1))
     cross_entropy_mean = tf.reduce_mean (cross_entropy)
     loss = cross_entropy_mean + tf.add_n (tf.get_collection ('losses'))
     learning_rate = tf.train.exponential_decay (
